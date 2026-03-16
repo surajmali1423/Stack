4229a0f607317b0619c4c8ad98c2cbcf13e9f8b3
surajmali1423 <malisuraj1423@gmail.com>
2026-03-16 20:47:29 +0530

    20. Valid Parentheses : Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.
    
    Example 1:
    
    Input: s = "()"
    
    Output: true
    
    Example 2:
    
    Input: s = "()[]{}"
    
    Output: true
    
    Example 3:
    
    Input: s = "(]"
    
    Output: false
    
    Example 4:
    
    Input: s = "([])"
    
    Output: true
    
    Example 5:
    
    Input: s = "([)]"
    
    Output: false
    
    Link : https://leetcode.com/problems/valid-parentheses/description/

---
+++ StackBasic/src/LeetcodeQuestion/Valid_Parentheses.java
@@ -0,0 +1,27 @@
+package LeetcodeQuestion;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static boolean isBalance(String str){
+        Stack<Character>st = new Stack<>();
+
+        for(char ch : str.toCharArray()){
+             if(ch == '(' || ch == '{' || ch == '[') st.push(ch);
+
+             else if(ch == ')' || ch == '}' || ch == ']') {
+                  if(st.isEmpty()) return false;
+                  char top = st.peek();
+
+                  if((top == '(' && ch == ')') || (top == '[' && ch == ']') || (top == '{' && ch == '}')) st.pop();
+            }
+
+             else return false;
+        }
+
+          return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        System.out.println(isBalance("{{(([[]][]))}}"));
+    }
+}
