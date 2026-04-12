96c34f22899b9c36f60be71e5ecee3e423a0ba68
surajmali1423 <malisuraj1423@gmail.com>
2026-04-12 19:45:22 +0530

    84. Largest Rectangle in Histogram :
    Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
    
    Example 1:
    
    Input: heights = [2,1,5,6,2,3]
    Output: 10
    Explanation: The above is a histogram where width of each bar is 1.
    The largest rectangle is shown in the red area, which has an area = 10 units.
    Example 2:
    
    Input: heights = [2,4]
    Output: 4
    
    Constraints:
    
    1 <= heights.length <= 105
    0 <= heights[i] <= 104
    
    Link : https://leetcode.com/problems/largest-rectangle-in-histogram/

---
+++ StackBasic/src/LeetcodeQuestion/Largest_Reactangle_in_Histogram.java
@@ -0,0 +1,54 @@
+package LeetcodeQuestion;
+
+import java.util.Stack;
+
+public class Largest_Reactangle_in_Histogram {
+    public static int [] PSE(int [] arr){
+        int n = arr.length;
+        Stack<Integer> st =   new Stack<>();
+        int []  ans = new int[n];
+
+        for(int i = 0 ; i < n; i++){
+             while (!st.isEmpty() && arr[i] < arr[st.peek()]) st.pop();
+             ans[i] = st.isEmpty() ? -1 : st.peek();
+             st.push(i);
+        }
+
+        return ans;
+    }
+
+    public static int [] NSE(int [] arr){
+        int n = arr.length;
+        Stack<Integer> st =   new Stack<>();
+        int []  ans = new int[n];
+
+        for(int i = n-1 ; i >= 0; i--){
+            while (!st.isEmpty() && arr[i] <= arr[st.peek()]) st.pop();
+            ans[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    public static int LargestRectangle(int [] arr) {
+
+        int n = arr.length;
+        int[] P = PSE(arr);
+        int[] N = NSE(arr);
+        int MaxArea = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int ans = N[i] - P[i] - 1;
+            int area = arr[i] * ans;
+            MaxArea = Math.max(MaxArea , area);
+        }
+
+        return MaxArea;
+    }
+        public static void main (String[]args){
+            int[] arr = {2 ,  4};
+            System.out.println("Max Area Is : "+LargestRectangle(arr));
+        }
+
+}
